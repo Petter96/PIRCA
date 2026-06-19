6a20d53afaa7b3223bab8f3856aa4dc7bcb4377f
Petter96 <pedrojose_2605@hotmail.com>
2026-06-19 00:49:27 -0600

    fukn CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: FRONTEND_URL,
     credentials: true
 }));
 app.use(morgan('dev'));
